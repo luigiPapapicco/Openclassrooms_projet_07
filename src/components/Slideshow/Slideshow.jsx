3612eac8b62ba1fed665236e7979541d440fa0ac
Luigi Papapicco <luigi.papapicco.pro@gmail.com>
2024-07-09 10:08:35 +0200

    refactorisation de slideshow pour l_affichage des chevrons uniquement quand y a des images

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -1,43 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Slideshow.module.scss'
 import chevronRight from '../../assets/chevrons/bigArrowRight.svg'
 import chevronLeft from '../../assets/chevrons/bigArrowLeft.svg'
 
 function Slideshow({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [transitioning, setTransitioning] = useState(false)
+
+  useEffect(() => {
+    if (transitioning) {
+      const timeout = setTimeout(() => {
+        setTransitioning(false)
+      }, 500) // Duration of the CSS transition
+      return () => clearTimeout(timeout)
+    }
+  }, [transitioning])
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    if (!transitioning) {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+      setTransitioning(true)
+    }
   }
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    )
+    if (!transitioning) {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      )
+      setTransitioning(true)
+    }
   }
 
   return (
     <div className={styles.slideshowContainer}>
-      <img
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex + 1}`}
-        className={styles.imgTst}
-      />
-      <div className={styles.counter}>
-        {currentIndex + 1} / {images.length}
-      </div>
-      <img
-        src={chevronLeft}
-        alt="symbol pour aller vers la gauche"
-        className={styles.chevronLeft}
-        onClick={handlePrev}
-      />
-      <img
-        src={chevronRight}
-        alt="symbol pour aller vers la droite"
-        className={styles.chevronRight}
-        onClick={handleNext}
-      />
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Slide ${index + 1}`}
+          className={`${styles.imgTst} ${
+            index === currentIndex ? 'active' : ''
+          }`}
+          style={{ opacity: index === currentIndex ? 1 : 0 }}
+        />
+      ))}
+      {images.length > 1 && (
+        <>
+          <div className={styles.counter}>
+            {currentIndex + 1} / {images.length}
+          </div>
+          <img
+            src={chevronLeft}
+            alt="symbol pour aller vers la gauche"
+            className={styles.chevronLeft}
+            onClick={handlePrev}
+          />
+          <img
+            src={chevronRight}
+            alt="symbol pour aller vers la droite"
+            className={styles.chevronRight}
+            onClick={handleNext}
+          />
+        </>
+      )}
     </div>
   )
 }
